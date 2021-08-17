2c50ee65186e4b8e2272a84f8d81a463cfd27ae1
Louise Hamoy <louise.thamoy@gmail.com>
2021-08-17 12:29:25 -0300

    Add tests of algorithms methods

--- mycode.py
+++ mycode.py
@@ -1,2 +1,11 @@
 def hello_world():
-  pass
+  return 'hello_world'
+
+def create_num_list(length):
+  return [x for x in range(length)]
+
+def custom_func_x(x, const, power):
+  return const * (x) ** power
+
+def custom_non_lin_num_list(length, const, power):
+  return [custom_func_x(x, const, power) for x in range(length)]
